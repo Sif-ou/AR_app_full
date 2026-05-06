914344e5434c88ac4fb5f086f6df4508e67189a4
sifou <sifou25@gmail.com>
2026-05-06 19:43:34 +0200

    fix chat_bot back-end

--- Server/demo/src/main/java/com/example/demo/Chat_Bot/ChatController.java
+++ Server/demo/src/main/java/com/example/demo/Chat_Bot/ChatController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/chat")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = {"Content-Type", "Accept"}, methods = {RequestMethod.POST, RequestMethod.OPTIONS})
 public class ChatController {
 
     private final AI_ModelService AI_Service;
