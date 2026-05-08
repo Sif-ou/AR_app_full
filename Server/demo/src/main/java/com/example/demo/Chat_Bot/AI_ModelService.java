6deac7bef58b349f264d96797d149ec01e844aec
sifou <sifou25@gmail.com>
2026-05-08 02:15:27 +0200

    fix backend chatbot

--- Server/demo/src/main/java/com/example/demo/Chat_Bot/AI_ModelService.java
+++ Server/demo/src/main/java/com/example/demo/Chat_Bot/AI_ModelService.java
@@ -37,7 +37,7 @@ public class AI_ModelService {
                 systemInstruction
         );
 
-    String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=" + apiKey;
+    String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + apiKey;
 
         return restClient.post()
                 .uri(url)
