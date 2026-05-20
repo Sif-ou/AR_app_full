f9fedb3aa37f5cd6f636fc52f5d1aaf0d6cd1c91
sifou <sifou25@gmail.com>
2026-05-21 01:08:56 +0200

    add google sing in

--- Client/components/GoogleAuthProvider.tsx
+++ Client/components/GoogleAuthProvider.tsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export function GoogleAuthProvider({ children }: { children: React.ReactNode }) {
   // Paste your actual Google Client ID here
-  const clientId = "YOUR_ACTUAL_COPIED_CLIENT_ID_HERE.apps.googleusercontent.com";
+  const clientId = "987952981330-rd2224he91nnobm8sd9fdeegcpj9e2t7.apps.googleusercontent.com";
 
   return (
     <GoogleOAuthProvider clientId={clientId}>
